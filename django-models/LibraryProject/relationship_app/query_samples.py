46f4a28c2845d77c92f0be33ffa2985f38cfa8b4
Gleta Kijogi <gkkijogi@gmail.com>
2026-01-18 13:05:54 +0300

    add django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,8 +3,9 @@ author = Author.objects.get(name = "Dostoevsky")
 books_by_author = Book.objects.filter(author = author)
 
 # List all books in a library
+library_name = "Main Library"
 library = Library.objects.get(name = "Main Library")
-books_in_library = Library.objects.filter(library = library)
+books = library.books.all()
 
 #librarian
 librarian = Librarian.objects.get(library = library)
