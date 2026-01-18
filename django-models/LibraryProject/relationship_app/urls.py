26ebe7816fa71206886d7513f000c1d296dbc1f4
Gleta Kijogi <gkkijogi@gmail.com>
2026-01-18 16:30:22 +0300

    add django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -26,3 +26,12 @@ urlpatterns = [
         name="register",
     ),
 ]
+
+from . import views
+
+urlpatterns = [
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+]
+
