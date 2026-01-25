f92e5e68f8c0815b34fb469ded1ef7de6c788356
Gleta Kijogi <gkkijogi@gmail.com>
2026-01-25 17:17:16 +0300

    add advanced_features_and_security

---
+++ advanced_features_and_security/LibraryProject/bookshelf/README.md
@@ -0,0 +1,19 @@
+## Permissions and Groups Setup
+
+This project uses Django permissions and groups to control access.
+
+### Custom Permissions
+Defined on the Book model:
+- can_view
+- can_create
+- can_edit
+- can_delete
+
+### Groups
+- Viewers: can_view
+- Editors: can_view, can_create, can_edit
+- Admins: all permissions
+
+### Enforcement
+Permissions are enforced in views using the
+@permission_required decorator with raise_exception=True.
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # Create your views here.
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    # logic to create book
+    return render(request, 'bookshelf/book_form.html')
+
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, book_id):
+    # logic to edit book
+    return render(request, 'bookshelf/book_form.html')
+
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, book_id):
+    # logic to delete book
+    return render(request, 'bookshelf/book_confirm_delete.html')
