0250cf74a6982c501227b5f31f0799740a636281
Gleta Kijogi <gkkijogi@gmail.com>
2026-02-22 23:15:59 +0300

    modified views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,61 +1,15 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions, filters
-from rest_framework.pagination import PageNumberPagination
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-
-# Create your views here.
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission: only owner can edit/delete
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
-
-
-class PostPagination(PageNumberPagination):
-    page_size = 5
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    pagination_class = PostPagination
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def feed(request):
-    following_users = request.user.following.all()
+class PostFeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    posts = Post.objects.filter(
-        author__in=following_users
-    ).order_by('-created_at')
+    def get_queryset(self):
+        following_users = self.request.user.following.all()
 
-    serializer = PostSerializer(posts, many=True)
-
-    return Response(serializer.data)
+        return Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')
