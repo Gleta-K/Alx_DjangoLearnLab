0025d32dfddcb998a411e18de23fbb4dd73e92cc
Gleta Kijogi <gkkijogi@gmail.com>
2026-02-22 23:01:12 +0300

    add social_media_api

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
-from .views import follow_user, unfollow_user
-
+from .views import FollowUserView, UnfollowUserView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name = "Register"),
     path('login/', LoginView.as_view(), name = "Login"),
     path('profile/', ProfileView.as_view(), name = "Profile"),
-    path('follow/<int:user_id>/', follow_user, name='follow-user'),
-    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user')
 ]
