3cfb972fafa23d196d53db988cec702d4292d8bc
Gleta Kijogi <gkkijogi@gmail.com>
2026-02-22 22:57:24 +0300

    implementing user follows and feed functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
+from .views import follow_user, unfollow_user
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name = "Register"),
     path('login/', LoginView.as_view(), name = "Login"),
-    path('profile/', ProfileView.as_view(), name = "Profile")
+    path('profile/', ProfileView.as_view(), name = "Profile"),
+    path('follow/<int:user_id>/', follow_user, name='follow-user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -31,3 +31,45 @@ class LoginView(APIView):
             token, created = Token.objects.get_or_create(user=user)
             return Response({"token": token.key})
         return Response({"error": "Invalid Credentials"}, status=400)
+    
+    from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework import status
+
+User = get_user_model()
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+
+        if user_to_follow == request.user:
+            return Response({"error": "You cannot follow yourself."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user_to_follow.followers.add(request.user)
+
+        return Response({"message": "User followed successfully."})
+
+    except User.DoesNotExist:
+        return Response({"error": "User not found."},
+                        status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+
+        user_to_unfollow.followers.remove(request.user)
+
+        return Response({"message": "User unfollowed successfully."})
+
+    except User.DoesNotExist:
+        return Response({"error": "User not found."},
+                        status=status.HTTP_404_NOT_FOUND)
